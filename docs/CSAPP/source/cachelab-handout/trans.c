38c0e201cd88210a5dfd2e2d785427e6615ab75e
liusy58 <mg21330037@smail.nju.edu.cn>
2022-02-07 21:20:51 +0800

    update on 2022-02-07 21:20:51

--- docs/CSAPP/source/cachelab-handout/trans.c
+++ docs/CSAPP/source/cachelab-handout/trans.c
@@ -22,6 +22,13 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+
+}
+
+char transpose_32_desc[] = "Transpose 32*32";
+void transpose_32(int M, int N, int A[N][M], int B[M][N])
+{
+    
 }
 
 /* 
@@ -56,7 +63,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    // registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
     registerTransFunction(trans, trans_desc); 
