71818fd3214a43c7bd7556664b52059c4776cbe4
liusy58 <mg21330037@smail.nju.edu.cn>
2022-02-09 15:01:03 +0800

    update on 2022-02-09 15:01:03

--- docs/CSAPP/source/cachelab-handout/driver.py
+++ docs/CSAPP/source/cachelab-handout/driver.py
@@ -1,4 +1,4 @@
-#!/usr//bin/python
+#!/usr//bin/python2
 #
 # driver.py - The driver tests the correctness of the student's cache
 #     simulator and the correctness and performance of their transpose
@@ -48,8 +48,8 @@ def main():
     autograde = opts.autograde
 
     # Check the correctness of the cache simulator
-    print "Part A: Testing cache simulator"
-    print "Running ./test-csim"
+    print ("Part A: Testing cache simulator")
+    print ("Running ./test-csim")
     p = subprocess.Popen("./test-csim", 
                          shell=True, stdout=subprocess.PIPE)
     stdout_data = p.communicate()[0]
@@ -60,26 +60,26 @@ def main():
         if re.match("TEST_CSIM_RESULTS", line):
             resultsim = re.findall(r'(\d+)', line)
         else:
-            print "%s" % (line)
+            print ("%s" % (line))
 
     # Check the correctness and performance of the transpose function
     # 32x32 transpose
-    print "Part B: Testing transpose function"
-    print "Running ./test-trans -M 32 -N 32"
+    print ("Part B: Testing transpose function")
+    print ("Running ./test-trans -M 32 -N 32")
     p = subprocess.Popen("./test-trans -M 32 -N 32 | grep TEST_TRANS_RESULTS", 
                          shell=True, stdout=subprocess.PIPE)
     stdout_data = p.communicate()[0]
     result32 = re.findall(r'(\d+)', stdout_data)
     
     # 64x64 transpose
-    print "Running ./test-trans -M 64 -N 64"
+    print ("Running ./test-trans -M 64 -N 64")
     p = subprocess.Popen("./test-trans -M 64 -N 64 | grep TEST_TRANS_RESULTS", 
                          shell=True, stdout=subprocess.PIPE)
     stdout_data = p.communicate()[0]
     result64 = re.findall(r'(\d+)', stdout_data)
     
     # 61x67 transpose
-    print "Running ./test-trans -M 61 -N 67"
+    print ("Running ./test-trans -M 61 -N 67")
     p = subprocess.Popen("./test-trans -M 61 -N 67 | grep TEST_TRANS_RESULTS", 
                          shell=True, stdout=subprocess.PIPE)
     stdout_data = p.communicate()[0]
@@ -97,39 +97,39 @@ def main():
     total_score = csim_cscore[0] + trans32_score + trans64_score + trans61_score
 
     # Summarize the results
-    print "\nCache Lab summary:"
-    print "%-22s%8s%10s%12s" % ("", "Points", "Max pts", "Misses")
-    print "%-22s%8.1f%10d" % ("Csim correctness", csim_cscore[0], 
-                              maxscore['csim'])
+    print ("\nCache Lab summary:")
+    print ("%-22s%8s%10s%12s" % ("", "Points", "Max pts", "Misses"))
+    print ("%-22s%8.1f%10d" % ("Csim correctness", csim_cscore[0], 
+                              maxscore['csim']))
 
     misses = str(miss32)
     if miss32 == 2**31-1 :
         misses = "invalid"
-    print "%-22s%8.1f%10d%12s" % ("Trans perf 32x32", trans32_score, 
-                                  maxscore['trans32'], misses)
+    print ("%-22s%8.1f%10d%12s" % ("Trans perf 32x32", trans32_score, 
+                                  maxscore['trans32'], misses))
 
     misses = str(miss64)
     if miss64 == 2**31-1 :
         misses = "invalid"
-    print "%-22s%8.1f%10d%12s" % ("Trans perf 64x64", trans64_score, 
-                                  maxscore['trans64'], misses)
+    print ("%-22s%8.1f%10d%12s" % ("Trans perf 64x64", trans64_score, 
+                                  maxscore['trans64'], misses))
 
     misses = str(miss61)
     if miss61 == 2**31-1 :
         misses = "invalid"
-    print "%-22s%8.1f%10d%12s" % ("Trans perf 61x67", trans61_score, 
-                                  maxscore['trans61'], misses)
+    print ("%-22s%8.1f%10d%12s" % ("Trans perf 61x67", trans61_score, 
+                                  maxscore['trans61'], misses))
 
-    print "%22s%8.1f%10d" % ("Total points", total_score,
+    print ("%22s%8.1f%10d" % ("Total points", total_score,
                              maxscore['csim'] + 
                              maxscore['trans32'] + 
                              maxscore['trans64'] +
-                             maxscore['trans61'])
+                             maxscore['trans61']))
     
     # Emit autoresult string for Autolab if called with -A option
     if autograde:
         autoresult="%.1f:%d:%d:%d" % (total_score, miss32, miss64, miss61)
-        print "\nAUTORESULT_STRING=%s" % autoresult
+        print ("\nAUTORESULT_STRING=%s" % autoresult)
     
     
 # execute main only if called as a script
--- docs/CSAPP/source/cachelab-handout/trans.c
+++ docs/CSAPP/source/cachelab-handout/trans.c
@@ -13,7 +13,9 @@
 #define min(a,b) (((a) < (b)) ? (a) : (b))
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
-
+void transpose_32_32(int M, int N, int A[N][M], int B[M][N]);
+void transpose_64_64(int M, int N, int A[N][M], int B[M][N]);
+void transpose_61_67(int M, int N, int A[N][M], int B[M][N]);
 /* 
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
@@ -36,12 +38,50 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    // int a0,a1,a2,a3,a4,a5,a6,a7;
+    // int i,j;
+    // int offset = 8;
+    // for(i=0;i<N;i=min(i+offset,N)){
+    //     for(j=0;j<M;j=min(j+offset,M)){
+    //         for(int k=i;k<min(i+offset,N);k++){
+    //             a0 = A[k][j];
+    //             a1 = A[k][j+1];
+    //             a2 = A[k][j+2];
+    //             a3 = A[k][j+3];
+    //             a4 = A[k][j+4];
+    //             a5 = A[k][j+5];
+    //             a6 = A[k][j+6];
+    //             a7 = A[k][j+7];
+    //             B[j][k] = a0;
+    //             B[j+1][k] = a1;
+    //             B[j+2][k] = a2;
+    //             B[j+3][k] = a3;
+    //             B[j+4][k] = a4;
+    //             B[j+5][k] = a5;
+    //             B[j+6][k] = a6;
+    //             B[j+7][k] = a7;
+    //         }
+    //     }
+    // }
+    if(M == 32 && N == 32)
+        transpose_32_32(M, N, A, B);    
+    else if(M == 64 && N == 64)
+        transpose_64_64(M, N, A, B);
+	else if(M == 61 && N == 67)
+		transpose_61_67(M, N, A, B);
+}
+
+
+// char transpose_32_32_desc[] = "Transpose submission";
+void transpose_32_32(int M, int N, int A[N][M], int B[M][N])
+{
     int a0,a1,a2,a3,a4,a5,a6,a7;
     int i,j;
     int offset = 8;
     for(i=0;i<N;i=min(i+offset,N)){
         for(j=0;j<M;j=min(j+offset,M)){
             for(int k=i;k<min(i+offset,N);k++){
+                // helped by registers
                 a0 = A[k][j];
                 a1 = A[k][j+1];
                 a2 = A[k][j+2];
@@ -63,6 +103,72 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
     }
 }
 
+// char transpose_61_67_desc[] = "Transpose submission";
+void transpose_61_67(int M, int N, int A[N][M], int B[M][N])
+{
+    int i,j,m,n;
+    int offset = 16;
+    for(i=0;i<N;i=min(i+offset,N)){
+        for(j=0;j<M;j=min(j+offset,M)){                  
+            for(m = i;m<min(i+offset,N);++m){
+                for(n = j;n<min(j+offset,M);++n){
+                    B[n][m] = A[m][n];
+                }
+            }
+        }
+    }
+}
+
+
+
+// char transpose_64_64_desc[] = "Transpose submission";
+void transpose_64_64(int M, int N, int A[N][M], int B[M][N])
+{
+    int i,j,m,k;
+    int a0,a1,a2,a3,a4,a5,a6,a7,tmp;
+    int offset = 8;
+    for(i=0;i<N;i=min(i+offset,N)){
+        for(j=0;j<M;j=min(j+offset,M)){                  
+            for(m=0;m<offset/2;m++){
+                a0 = A[i+m][j];
+                a1 = A[i+m][j+1];
+                a2 = A[i+m][j+2];
+                a3 = A[i+m][j+3];
+                a4 = A[i+m][j+4];
+                a5 = A[i+m][j+5];
+                a6 = A[i+m][j+6];
+                a7 = A[i+m][j+7];
+
+                B[j+0][i+m] = a0;
+                B[j+1][i+m] = a1;
+                B[j+2][i+m] = a2;
+                B[j+3][i+m] = a3;
+
+                B[j+0][i+m+offset/2] = a4;
+                B[j+1][i+m+offset/2] = a5;
+                B[j+2][i+m+offset/2] = a6;
+                B[j+3][i+m+offset/2] = a7;
+            }
+            for(k=0;k<offset/2;k++){
+                // step 1 2
+                a0 = A[i + 4][j + k], a4 = A[i + 4][j + k + 4];
+                a1 = A[i + 5][j + k], a5 = A[i + 5][j + k + 4];
+                a2 = A[i + 6][j + k], a6 = A[i + 6][j + k + 4];
+                a3 = A[i + 7][j + k], a7 = A[i + 7][j + k + 4];
+                // step 3
+                tmp = B[j + k][i + 4], B[j + k][i + 4] = a0, a0 = tmp;
+                tmp = B[j + k][i + 5], B[j + k][i + 5] = a1, a1 = tmp;
+                tmp = B[j + k][i + 6], B[j + k][i + 6] = a2, a2 = tmp;
+                tmp = B[j + k][i + 7], B[j + k][i + 7] = a3, a3 = tmp;
+                // step 4
+                B[j + k + 4][i + 0] = a0, B[j + k + 4][i + 4 + 0] = a4;
+                B[j + k + 4][i + 1] = a1, B[j + k + 4][i + 4 + 1] = a5;
+                B[j + k + 4][i + 2] = a2, B[j + k + 4][i + 4 + 2] = a6;
+                B[j + k + 4][i + 3] = a3, B[j + k + 4][i + 4 + 3] = a7;
+            }
+        }
+    }
+}
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
@@ -96,10 +202,11 @@ void registerFunctions()
 {
     /* Register your solution function */
     registerTransFunction(transpose_submit, transpose_submit_desc); 
-
-    /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-    // registerTransFunction(transpose_32, transpose_32_desc); 
+    // registerTransFunction(transpose_61_67, transpose_61_67_desc); 
+    // registerTransFunction(transpose_32_32, transpose_32_32_desc); 
+    // /* Register any additional transpose functions */
+    // registerTransFunction(trans, trans_desc); 
+    // // registerTransFunction(transpose_32, transpose_32_desc); 
 }
 
 /* 
